ef8cf35de06ebdccedf91ac2db7045127a16715f
Yuri Kim <kkyuri0419@gmail.com>
2026-04-24 23:03:35 +0200

    adds seasonal differencing

--- scripts/01_data_preprocessing.R
+++ scripts/01_data_preprocessing.R
@@ -14,14 +14,22 @@ data <- ts(rawData$OBS_VALUE,start = c(2005,1), frequency = 12)
 
 
 # differentiate the data to remove the trend
-diff_data <- diff(data)
-df_g <- autoplot(diff_data)
-df_g
-ggsave("graphs/data_differentiation_visualization.png", plot = df_g, width = 8, height = 5, dpi = 300)
 
-#Although the series has been differenced, the variance is still increasing over time, 
-#so it is not suitable for modeling yet — a log transformation is needed
+
+# Apply log transformation to stabilize the variance
 log_data <- log(data)
 log_g <- autoplot(log_data)
 log_g
 ggsave("graphs/data_log_transformed_visualization.png", plot = log_g, width = 8, height = 5, dpi = 300)
+
+# Apply differencing to remove the trend
+diff_log_data <- diff(log_data)
+df_g <- autoplot(diff_log_data)
+df_g
+ggsave("graphs/data_diff_log_visualization.png", plot = df_g, width = 8, height = 5, dpi = 300)
+
+# Apply seasonal differencing to remove the seasonality
+diff_seasonal <- diff(diff_log_data, lag=12)
+df_s_g <- autoplot(diff_seasonal)
+df_s_g
+ggsave("graphs/data_diff_seasonal_visualization.png", plot = df_s_g, width = 8, height = 5, dpi = 300)
